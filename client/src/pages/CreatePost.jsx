2838e59db0186eda55e9c45d9b3680d1a6b25209
ChaitanyaPimpare <chaitanyapimpare2003@gmail.com>
2025-08-04 23:46:19 +0530

    Updated CreatePost page and added README.md

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,6 +17,7 @@ const CreatePost = () => {
         { text },
         {
           headers: {
+            "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
           },
         }
