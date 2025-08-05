3bb09245d6bc0935327d90fa469ae5174abe9a09
ChaitanyaPimpare <chaitanyapimpare2003@gmail.com>
2025-08-05 12:41:55 +0530

    updated routes

--- server/user_routes.py
+++ server/user_routes.py
@@ -3,7 +3,7 @@ from models import User, Post
 
 user_bp = Blueprint('user', __name__)
 
-@user_bp.route('/user/<int:user_id>', methods=['GET'])
+@user_bp.route('<int:user_id>', methods=['GET'])
 def get_user_profile(user_id):
     user = User.query.get(user_id)
     if not user:
