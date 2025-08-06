2ffa1ef4f319865731c92a2c78719e434c5dfa93
ChaitanyaPimpare <chaitanyapimpare2003@gmail.com>
2025-08-07 00:20:02 +0530

    updated login route

--- server/auth_routes.py
+++ server/auth_routes.py
@@ -44,7 +44,7 @@ def login():
     user = User.query.filter_by(email=email).first()
 
     if user and bcrypt.check_password_hash(user.password, password):
-        access_token = create_access_token(identity=user.id)
+        access_token = create_access_token(identity=str(user.id))
         return jsonify({
             'access_token': access_token,
             'message': 'Login successful',
